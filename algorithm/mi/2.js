710b3f0597a08e85ab6dca804b356fbd2effa77f
PoleWick <2811293936@qq.com>
2024-12-04 21:30:10 +0800

    setTimeout

--- algorithm/mi/2.js
+++ algorithm/mi/2.js
@@ -3,8 +3,6 @@ function powerUsingRecursion(x, n) {
         return 1;
     } else if (n > 0) {
         return x * powerUsingRecursion(x, n - 1);
-    } else {
-        return (1 / x) * powerUsingRecursion(x, n + 1);
-    }
+    } 
 }
 console.log(powerUsingRecursion(2, 3));
---
+++ algorithm/mi/4.js
@@ -0,0 +1,5 @@
+// 思路：使用 JavaScript 内置的 Math.pow() 函数，它接受两个参数，第一个参数是底数 x，第二个参数是指数 n，返回 x 的 n 次方。
+function powerUsingMathPow(x, n) {
+    return Math.pow(x, n);
+}
+console.log(powerUsingMathPow(2, 3));
