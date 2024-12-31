31bc00255d1e9b9108065451ebb3de075f30861d
PoleWick <2811293936@qq.com>
2024-12-31 17:06:45 +0800

    rate

---
+++ vue_react/reactive/rate/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+header{
+    height: 100px;
+    background-color: lightblue;
+
+}
+</style>
