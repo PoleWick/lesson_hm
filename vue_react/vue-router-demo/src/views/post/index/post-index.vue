1a9b30dc383f832822bae58ae0de003ecf1f34e0
PoleWick <2811293936@qq.com>
2025-01-03 21:27:52 +0800

    router

---
+++ vue_react/vue-router-demo/src/views/post/index/post-index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        PostIndex
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute() // 当前路由
+// console.log(route);
+const { postId } = route.params // 参数对象
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_react/vue-router-demo/src/views/post/index/post-show.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>PostShow{{postId}}</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
