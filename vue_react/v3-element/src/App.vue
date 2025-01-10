7417f839cb8082f4d050ef2ff369dcf42a3015b8
PoleWick <2811293936@qq.com>
2025-01-10 16:42:02 +0800

    element3引入和路由配置

---
+++ vue_react/v3-element/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <el-button type="primary">主要按钮</el-button>
+    <routerview />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue_react/v3-element/src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        About
+    </div>
+</template>
+
+<script setup>
+console.log('about');
+
+</script>
+
+<style scoped>
+
+</style>
